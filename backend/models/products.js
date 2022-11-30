65b261f3b9e9c4840587dcff628839097fdd8ca2
Palisha Shakya <palishashakya@gmail.com>
2022-11-30 10:54:27 +0545

    connected to mysql with sequelize orm and product schema created

---
+++ backend/models/products.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, Sequelize) => {
+  const productSchema = sequelize.define("Products", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    product_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    category_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    created_by: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.ENUM("In Stock", "Out of Stock"),
+      defaultValue: "In Stock",
+    },
+
+    createdAt: {
+      field: "created_at",
+      type: Sequelize.DATE,
+      defaultValue: sequelize.fn("NOW"),
+      allowNull: false,
+    },
+    updatedAt: {
+      field: "updated_at",
+      type: Sequelize.DATE,
+      defaultValue: sequelize.fn("NOW"),
+      allowNull: false,
+    },
+  });
+  return productSchema;
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const db = require("./models/index");
+
 const app = express();
 app.use(express.json());
 dotenv.config();
@@ -10,6 +12,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port :http://localhost:${PORT}`)
-);
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server running on port :http://localhost:${PORT}`)
+  );
+});
